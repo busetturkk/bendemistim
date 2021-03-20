03330c2cc78a275abc4e93aa8d878e1aa47fe894
Buse Türk <busetturkk@gmail.com>
2021-03-20 23:31:12 +0300

    Update main.dart
    color fixed

--- lib/pages/login/main.dart
+++ lib/pages/login/main.dart
@@ -9,139 +9,177 @@ void main() {
 }
 
 class HomePage extends StatelessWidget {
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         width: double.infinity,
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            colors: [
-              Colors.blue[900],
-              Colors.blue[400],
-              Colors.blue[400]
-            ]
-          )
-        ),
+            gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+          Colors.yellow[800],
+          Colors.yellow[400],
+          Colors.yellow[400]
+        ])),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children:<Widget>[
-            SizedBox(height: 80,),
+          children: <Widget>[
+            SizedBox(
+              height: 80,
+            ),
             Padding(
-                padding: EdgeInsets.all(20),
-                child : Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children:<Widget>[
-                    Text("Login", style:TextStyle(color: Colors.white, fontSize: 40),),
-                    SizedBox(height: 10,),
-                    Text("Welcome Back", style:TextStyle(color: Colors.white, fontSize: 18),),
-              ],
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Login",
+                    style: TextStyle(color: Colors.white, fontSize: 40),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Welcome Back",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ],
+              ),
             ),
+            SizedBox(
+              height: 20,
             ),
-            SizedBox(height: 20,),
             Expanded(
                 child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(topLeft: Radius.circular(60),topRight: Radius.circular(60))
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.all(30),
-                    child: Column(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(60),
+                      topRight: Radius.circular(60))),
+              child: Padding(
+                padding: EdgeInsets.all(30),
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 60,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.grey,
+                                blurRadius: 20,
+                                offset: Offset(0, 10))
+                          ]),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                                border: Border(
+                                    bottom:
+                                        BorderSide(color: Colors.grey[200]))),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  hintText: "Email or Phone number",
+                                  hintStyle: TextStyle(color: Colors.grey),
+                                  border: InputBorder.none),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                                border: Border(
+                                    bottom:
+                                        BorderSide(color: Colors.grey[200]))),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  hintText: "Password",
+                                  hintStyle: TextStyle(color: Colors.grey),
+                                  border: InputBorder.none),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Text(
+                      "Forgot Password?",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Container(
+                      height: 50,
+                      margin: EdgeInsets.symmetric(horizontal: 50),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.amberAccent,
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      "Continue with social media",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Row(
                       children: <Widget>[
-                        SizedBox(height: 60,),
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [BoxShadow(
-                              color: Color.fromRGBO(11, 11, 149, .3),
-                              blurRadius: 20,
-                              offset: Offset(0,10)
-                            )]
-                          ),
-                          child: Column(
-                            children: <Widget>[
-                              Container(
-                                padding:EdgeInsets.all(10),
-                                decoration: BoxDecoration(
-                                  border: Border(bottom: BorderSide(color: Colors.grey[200]))
-                                ),
-                                child: TextField(
-                                  decoration: InputDecoration(
-                                    hintText: "Email or Phone number",
-                                    hintStyle: TextStyle(color: Colors.grey),
-                                    border: InputBorder.none
-                                  ),
-                                ),
-                              ),
-                              Container(
-                                padding:EdgeInsets.all(10),
-                                decoration: BoxDecoration(
-                                    border: Border(bottom: BorderSide(color: Colors.grey[200]))
-                                ),
-                                child: TextField(
-                                  decoration: InputDecoration(
-                                      hintText: "Password",
-                                      hintStyle: TextStyle(color: Colors.grey),
-                                      border: InputBorder.none
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        SizedBox(height: 40,),
-                        Text("Forgot Password?", style: TextStyle(color: Colors.grey),),
-                        SizedBox(height: 40,),
-                        Container(
-                          height: 50,
-                          margin: EdgeInsets.symmetric(horizontal: 50),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                            color: Colors.blue[900]
-                          ),
-                          child: Center(
-                            child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
-                          ),
-                        ),
-                        SizedBox(height: 30,),
-                        Text("Continue with social media", style: TextStyle(color: Colors.grey),),
-                        SizedBox(height: 30,),
-                        Row(
-                          children: <Widget>[
-                            Expanded(
-                            child: Container(
-                              height: 40,
-                              decoration: BoxDecoration(
+                        Expanded(
+                          child: Container(
+                            height: 40,
+                            decoration: BoxDecoration(
                                 borderRadius: BorderRadius.circular(50),
-                                color: Colors.blue
-                              ),
-                              child: Center(
-                                child: Text("Twitter", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
-                              ),
-                            ),
-                            ),
-                            SizedBox(width: 30,),
-                            Expanded(
-                            child : Container(
-                              height: 40,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(50),
-                                  color: Colors.red
-                              ),
-                              child: Center(
-                                child: Text("Google Accounts", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                                color: Colors.blue),
+                            child: Center(
+                              child: Text(
+                                "Twitter",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
                               ),
                             ),
-                            )
-                          ],
+                          ),
+                        ),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        Expanded(
+                          child: Container(
+                            height: 40,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                color: Colors.red),
+                            child: Center(
+                              child: Text(
+                                "Google Accounts",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
                         )
                       ],
-                    ),
-                  ),
-            )
-            )
+                    )
+                  ],
+                ),
+              ),
+            ))
           ],
         ),
       ),
